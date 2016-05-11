6baa94ad123ca355fc4a9b4e1843d83050c70b33
Gao, Shi <sgao@globeandmail.com>
2016-05-11 16:13:08 -0400

    add pattern guard

--- src/main/scala/com/h7d1w0/PattenMatchingExample.scala
+++ src/main/scala/com/h7d1w0/PattenMatchingExample.scala
@@ -27,50 +27,62 @@ object PattenMatchingExample {
     }
     println("result2:" + result2)
 
-    //
+    //variable pattern
+    val valueToTest2_2: Any = "abcd"
+    val result2_2 = valueToTest2_2 match {
+      case 1 => "Digital number one "
+      case Employee("Tom", 35) => "An Employee object"
+      case v => "other"
+    }
+    println("result2_2:" + result2_2)
+
+
+    //constructor pattern
     val valueToTest3: Any = emp1
     val result3 = valueToTest3 match {
       case 1 => "Digital number one "
       case "abcd" => "Alphabic sequence"
-      case variableCanBeReferredTo@Employee("Tom", 35) => "An Employee object:  " + variableCanBeReferredTo
+      case variableCanBeReferredTo @ Employee("Tom", 35) => "An Employee object:  " + variableCanBeReferredTo
       case _ => "other"
     }
     println("result3:" + result3)
 
+    //constructor pattern - with variable pattern of parameters
     val valueToTest4: Any = group
     val result4 = valueToTest4 match {
       case 1 => "Digital number one "
       case "abcd" => "Alphabic sequence"
       case Employee("Tom", 35) => "An Employee object"
-      case TwoEmployeeGroup(empNotExist1, empNotExist2) => empNotExist2.toString
+      case TwoEmployeeGroup(employee1, employee2) => "A name match: constructor pattern - with variable pattern for parameters"
       case _ => "other"
     }
     println("result4:" + result4)
 
+    //constructor pattern - with constant pattern of parameters
     val valueToTest5: Any = twoNames
     val result5 = valueToTest5 match {
       case 1 => "Digital number one "
       case "abcd" => "Alphabic sequence"
       case Employee("Tom", 35) => "An Employee object"
       case TwoEmployeeGroup(empNotExist1, empNotExist2) => "A group match"
-      case TwoEmployeeNames("Tom", "Jerry") => "A name match"
+      case TwoEmployeeNames("Tom", "Jerry") => "A name match: constructor pattern - with constant pattern for parameters"
       case _ => "other"
     }
     println("result5:" + result5)
 
-    //
+    //constructor pattern - with wildcard pattern of parameters
     val valueToTest6: Any = twoNames
     val result6 = valueToTest6 match {
       case 1 => "Digital number one "
       case "abcd" => "Alphabic sequence"
       case Employee("Tom", 35) => "An Employee object"
-      case TwoEmployeeGroup(empNotExist1, empNotExist2) => "A group match"
-      case TwoEmployeeNames(_, _) => "A name match"
+      case TwoEmployeeGroup(employee1, employee2) => "A group match"
+      case TwoEmployeeNames(_, _) => "A name match: constructor pattern - with wildcard pattern for parameters"
       case _ => "other"
     }
     println("result6:" + result6)
 
-    //
+    //Typed pattern
     val valueToTest7_1 = Array(1,2,3)
     println("valueToTest7_1: " + findType(valueToTest7_1))
     val valueToTest7_2 = 10
@@ -82,12 +94,22 @@ object PattenMatchingExample {
     val valueToTest7_5 = ("abc", "bcd" , "efg")
     println("valueToTest7_5: " + findType(valueToTest7_5))
 
-    //
+    //Typed pattern
     println("valueToTest7_4: " + findElementType(valueToTest7_4))
     println("valueToTest7_1: " + findElementType(valueToTest7_1))
 
     val valueToTest8 = Array("t","r","a","i","n")
     println("valueToTest8: " + findElementType(valueToTest8))
+
+
+    //Pattern guard
+    val impossibleGroup = TwoEmployeeGroup(emp1, emp1)
+    val result7 = impossibleGroup match {
+      case TwoEmployeeGroup(employee1, employee2) if employee1==employee2 => "This group is impossible"
+      case _ => "other"
+    }
+    println("result6:" + result7)
+
   }
 
   def findType(x: Any): String = {
